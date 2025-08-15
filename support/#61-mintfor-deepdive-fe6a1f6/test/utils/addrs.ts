c9c20fd5cb51f751c83a8f7f2d16901be09b0d61
yomarfrancisco <ygor.francisco@gmail.com>
2025-08-15 10:24:28 +0200

    chore: remove redundant support files from issue #61 investigation
    - Remove support/#61-mintfor-deepdive-fe6a1f6/ directory and contents
    - Remove support/issue61-bundle-README.md
    - Remove support/#61-mintfor-deepdive-fe6a1f6-*.txt/.tar files
    
    These files were created during issue #61 investigation and audit work.
    They are now redundant since:
    - support/ is now in .gitignore
    - Pre-commit guard prevents future support/ additions
    - Branch is cleaned up and hardened
    
    The investigation artifacts are no longer needed in the repository.

--- support/#61-mintfor-deepdive-fe6a1f6/test/utils/addrs.ts
+++
@@ -1,7 +0,0 @@
-export async function addr(c: any) { 
-  return c.getAddress(); 
-}
-
-export async function approve20(token: any, signer: any, spender: any, amt: bigint) {
-  return token.connect(signer).approve(await addr(spender), amt);
-}
